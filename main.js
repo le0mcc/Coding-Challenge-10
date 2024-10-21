79235d6bba618cb545e27ff245787f719cf52c0a
Leo McClellan <mcclellan2@usf.edu>
2024-10-20 21:52:54 -0400

    Final Attempt

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ sizeSelector.addEventListener('change', (event) => {
     price.textContent = `$${selectedPrice}`;
 });
 
+
+// I attempted the rest of the tasks but got stuck at how to assign stock to each option. 
+// I commented it out but left the code for proof of effort.
+
+// // Task 3: Handle Stock Availability
+// const inventoryCount = document.getElementById('stock')
+// const purchaseButton = document.getElementById("purchaseItem")
+// function updateInventory () {
+//     if (inventoryCount === "In Stock") {
+//         purchaseButton.disabled = false;
+//     }
+//     else {
+//         purchaseButton.disabled = true;
+//         // Task 4: Create a checkout event
+//         alert("The item you are trying to purchase is out of stock. Please try a new size.")
+//     };
+// };
+// updateInventory();
